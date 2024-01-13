ee70c65f1fe3f50286277211e30186121757b56c
Stanford User <oliviayl@stanford.edu>
2024-01-13 15:20:16 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# flutter
-Flutter: An LLM-powered simulated drone
+# Flutter: An LLM-powered simulated drone
 
 Our project consists of three main components:
 1. **[AirSim](https://microsoft.github.io/AirSim/), a simulator for drones, cars and other vehicles, built on Unreal Engine.**
     - We develop a custom AirSim environment with a simulated drone that can be controlled using natural language.
 
 2. **LLM integration: GPT-4**
-    - Using an LLM easily interprets natural language instructions, which are more intuitive than low-level action controllers, and introduces capabilities for multi-step reasoning and task planning.
+    - Using an LLM easily interprets natural language instructions, which are more intuitive than low-level action controllers. This introduces capabilities for multi-step reasoning and task planning.
 
 3. **Object Detection Model: Cloud Vision API**
     - Using a dedicated object detection model improves accuracy on understanding dynamic scenes and real-time mapping. This facilitates completing complex tasks by grounding the language model.
---
+++ airsim_wrapper.py
@@ -0,0 +1,221 @@
+import base64
+import json
+import math
+import os
+import random
+import threading
+import time
+
+import airsim
+import cv2
+import numpy as np
+import openai
+import requests
+from google.cloud import vision
+from inference import get_roboflow_model
+
+objects_dict = {
+    "turbine1": "BP_Wind_Turbines_C_1",
+    "turbine2": "StaticMeshActor_2",
+    "solarpanels": "StaticMeshActor_146",
+    "crowd": "StaticMeshActor_6",
+    "car": "StaticMeshActor_10",
+    "tower1": "SM_Electric_trellis_179",
+    "tower2": "SM_Electric_trellis_7",
+    "tower3": "SM_Electric_trellis_8",
+}
+
+
+class AirSimWrapper:
+    def __init__(self):
+        self.client = airsim.MultirotorClient()
+        self.client.confirmConnection()
+        self.client.enableApiControl(True)
+        self.client.armDisarm(True)
+        self.stop_thread = False
+        self.flutter_thread = None
+
+    def takeoff(self):
+        self.client.takeoffAsync().join()
+
+    def land(self):
+        self.client.landAsync().join()
+
+    def get_drone_position(self):
+        pose = self.client.simGetVehiclePose()
+        return [pose.position.x_val, pose.position.y_val, pose.position.z_val]
+
+    def fly_to(self, point):
+        if point[2] > 0:
+            self.client.moveToPositionAsync(point[0], point[1], -point[2], 5).join()
+        else:
+            self.client.moveToPositionAsync(point[0], point[1], point[2], 5).join()
+
+    def fly_path(self, points):
+        airsim_points = []
+        for point in points:
+            if point[2] > 0:
+                airsim_points.append(airsim.Vector3r(point[0], point[1], -point[2]))
+            else:
+                airsim_points.append(airsim.Vector3r(point[0], point[1], point[2]))
+        self.client.moveOnPathAsync(
+            airsim_points,
+            5,
+            120,
+            airsim.DrivetrainType.ForwardOnly,
+            airsim.YawMode(False, 0),
+            20,
+            1,
+        ).join()
+
+    def set_yaw(self, yaw):
+        self.client.rotateToYawAsync(yaw, 5).join()
+
+    def get_yaw(self):
+        orientation_quat = self.client.simGetVehiclePose().orientation
+        yaw = airsim.to_eularian_angles(orientation_quat)[2]
+        return yaw
+
+    def get_position(self, object_name):
+        query_string = objects_dict[object_name] + ".*"
+        object_names_ue = []
+        while len(object_names_ue) == 0:
+            object_names_ue = self.client.simListSceneObjects(query_string)
+        pose = self.client.simGetObjectPose(object_names_ue[0])
+        return [pose.position.x_val, pose.position.y_val, pose.position.z_val]
+
+    @staticmethod
+    def is_within_boundary(start_pos, current_pos, limit_radius):
+        """Check if the drone is within the spherical boundary"""
+        distance = math.sqrt(
+            (current_pos.x_val - start_pos.x_val) ** 2
+            + (current_pos.y_val - start_pos.y_val) ** 2
+            + (current_pos.z_val - start_pos.z_val) ** 2
+        )
+        return distance <= limit_radius
+
+    def flutter(self, speed=5, change_interval=1, limit_radius=10):
+        """Simulate Brownian motion /fluttering with the drone"""
+        # Takeoff and get initial position
+        self.client.takeoffAsync().join()
+        start_position = self.client.simGetVehiclePose().position
+
+        while not self.stop_thread:
+            # Propose a random direction
+            pitch = random.uniform(-1, 1)  # Forward/backward
+            roll = random.uniform(-1, 1)  # Left/right
+            yaw = random.uniform(-1, 1)  # Rotate
+
+            # Move the drone in the proposed direction
+            self.client.moveByRollPitchYawrateThrottleAsync(
+                roll, pitch, yaw, 0.5, change_interval
+            ).join()
+
+            # Get the current position
+            current_position = self.client.simGetVehiclePose().position
+
+            # Check if the drone is within the boundary
+            if not self.is_within_boundary(
+                start_position, current_position, limit_radius
+            ):
+                # If outside the boundary, adjust to a new random direction
+                self.client.moveToPositionAsync(
+                    start_position.x_val,
+                    start_position.y_val,
+                    start_position.z_val,
+                    speed,
+                ).join()
+
+            # Wait for the next change
+            time.sleep(change_interval)
+
+    def start_fluttering(self, speed=5, change_interval=1, limit_radius=10):
+        self.stop_thread = False
+        self.flutter_thread = threading.Thread(
+            target=self.flutter, args=(speed, change_interval, limit_radius)
+        )
+        self.flutter_thread.start()
+
+    def stop_fluttering(self):
+        self.stop_thread = True
+        if self.flutter_thread is not None:
+            self.flutter_thread.join()
+
+    def generate_circular_path(center, radius, height, segments=12):
+        path = []
+        for i in range(segments):
+            angle = 2 * math.pi * i / segments
+            x = center[0] + radius * math.cos(angle)
+            y = center[1] + radius * math.sin(angle)
+            z = height
+            path.append(x, y, z)
+        return path
+
+    def take_photo(self, filename="image.png"):
+        responses = self.client.simGetImages(
+            [airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)]
+        )
+        response = responses[0]
+        img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8)
+        img_rgb = img1d.reshape(response.height, response.width, 3)
+        filename = os.path.normpath(filename + ".png")
+        cv2.imwrite(filename, img_rgb)
+        with open(filename, "rb") as image_file:
+            base64_image = base64.b64encode(image_file.read()).decode("utf-8")
+        return base64_image
+
+    def analyze_with_vision_model(self, image_data):
+        # Google Vision API: https://cloud.google.com/vision/docs/object-localizer
+        client = vision.ImageAnnotatorClient()
+        content = image_data.read()
+        image = vision.Image(content=content)
+        objects = client.object_localization(image=image).localized_object_annotations
+        return objects
+
+    def query_language_model(self, prompt):
+        with open("config.json", "r") as f:
+            config = json.load(f)
+        openai.api_key = config["OPENAI_API_KEY"]
+        chat_history = [
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ]
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=chat_history, temperature=0
+        )
+        return completion.choices[0].message.content
+
+    # Complex Commands
+    def count(self, object_name):
+        # Converts vision model json output to string, append to count prompt
+        filename = "count" + object_name + ".jpg"
+        image_data = self.take_photo(filename)
+        vision_outputs = self.analyze_with_vision_model(image_data)
+        prompt = "\n Based on this json output, count the number of instances of " + object_name + " in the scene. Return a single number"
+        return self.query_language_model(str(vision_outputs) + prompt)
+
+    def search(self, object_name, radius):
+        # Fly to object, fly in a circle, take photos, check if object present
+        self.fly_to(self.get_position(object_name))
+        circular_path = self.generate_circular_path(
+            self.get_position(object_name)[:2],
+            radius,
+            self.get_position(object_name)[2],
+        )
+        vision_outputs = ""
+        for point in circular_path:
+            self.fly_to(point)
+            image_data = self.take_photo(str(point))
+            vision_output = self.analyze_with_vision_model(image_data)
+            vision_outputs += str(vision_output)
+        prompt = "\n Based on these json outputs, is " + object_name + " present in the scene? Return TRUE or FALSE."
+        return self.query_language_model(str(vision_outputs) + prompt)
+
+    def get_latitude_longitude(self, object_name):
+        self.fly_to(self.get_position(object_name))
+        return (
+            self.get_position(object_name)[0],
+            self.get_drone_position(object_name)[1],
+        )
